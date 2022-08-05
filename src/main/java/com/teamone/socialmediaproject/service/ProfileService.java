055e08541867cea0f2f490fa87dbc8abfc03403f
Conganh97 <anhbc.afsystem@gmail.com>
2022-08-05 15:57:19 +0700

    checkout conganh2

--- src/main/java/com/teamone/socialmediaproject/service/ProfileService.java
+++ src/main/java/com/teamone/socialmediaproject/service/ProfileService.java
@@ -25,9 +25,6 @@ public Profile findByName(String name){
         return iUserProfileRepo.findByAppUserUserName(name);
 }
 
-    public Profile findProfilebyIdUser (long id){
-        return iUserProfileRepo.findProfileByAppUserIdUser(id);
-    }
     public List<Profile>findAllByProfileNotFriends(long user,String name){
         return iUserProfileRepo.findAllByProfileNotFriends(user,name);
     }
