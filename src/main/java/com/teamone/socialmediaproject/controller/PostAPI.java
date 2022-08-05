fff0cc20261a72a27e96cdf253de642be2a92e55
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-08-05 15:46:07 +0700

    merge

--- src/main/java/com/teamone/socialmediaproject/controller/PostAPI.java
+++ src/main/java/com/teamone/socialmediaproject/controller/PostAPI.java
@@ -8,8 +8,15 @@ import com.teamone.socialmediaproject.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,6 +27,8 @@ public class PostAPI {
     PostService postService;
     @Autowired
     ProfileService profileService;
+    @Autowired
+    AppUserService appUserServicel;
 
     @GetMapping("/post")
     public ResponseEntity<List<Post>> getAllPostFriend() {
@@ -37,4 +46,24 @@ public class PostAPI {
         return new ResponseEntity<>(profileService.findProfilebyIdUser(postService.findIdUser()),HttpStatus.ACCEPTED);
     }
 
+    @PostMapping("/upImg")
+    public String upImg(@RequestParam MultipartFile file){
+        String name = file.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(file.getBytes(),new File("D:\\CodeGym\\CaseModul4\\Social\\assets\\images\\post\\imgpost/" + name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "/assets/images/post/imgpost/"+name;
+    }
+
+    @PostMapping("/createPost")
+    public Post save (@RequestBody Post post){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        post.setNumCommentPost(0);
+        post.setNumLikePost(0);
+        post.setTimePost(new Date());
+        post.setAppUser(appUserServicel.findByName(userDetails.getUsername()));
+        return postService.save(post);
+    }
 }
