7e12ddf206bf649e39ce18e732d543340f7db085
bieen <99934768+bienpacman@users.noreply.github.com>
2022-08-05 16:38:29 +0700

    up

--- src/main/java/com/teamone/socialmediaproject/controller/PostAPI.java
+++ src/main/java/com/teamone/socialmediaproject/controller/PostAPI.java
@@ -8,8 +8,12 @@ import com.teamone.socialmediaproject.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -36,5 +40,22 @@ public class PostAPI {
     public ResponseEntity<Profile> getProfile(){
         return new ResponseEntity<>(profileService.findProfilebyIdUser(postService.findIdUser()),HttpStatus.ACCEPTED);
     }
+
+//    ________________________________________________________
+@PostMapping
+public void upAvatar(@RequestBody Profile profile) {
+         profileService.save(profile);
+}
+@PostMapping("/upImg")
+        public String upImg(@RequestParam MultipartFile file) {
+    String name = file.getOriginalFilename();
+    try {
+        FileCopyUtils.copy(file.getBytes(),new File("C:\\Users\\phamv\\Downloads\\Font_End\\FE_SocialMedia_Case\\assets\\images\\"));
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return "/img/" + name;
+}
 
 }
--- src/main/java/com/teamone/socialmediaproject/model/Profile.java
+++ src/main/java/com/teamone/socialmediaproject/model/Profile.java
@@ -39,4 +39,5 @@ public class Profile {
         this.gender = gender;
         this.appUser = appUser;
 }
+
 }
