2c223a5eb0b965625bb10965c48873bea0acfc12
CodeXplore <thuhoi0205@gmail.com>
2022-08-06 14:02:30 +0700

    like complete

--- src/main/java/com/teamone/socialmediaproject/repository/ILikeRepo.java
+++ src/main/java/com/teamone/socialmediaproject/repository/ILikeRepo.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ILikeRepo extends CrudRepository<Likes, Long> {
- List findByAppUserAndAndPost(AppUser appUser, Post post);
- int countLikesByPost(Post post);
+ Likes findByAppUserAndAndPost(AppUser appUser, Post post);
 
 }
