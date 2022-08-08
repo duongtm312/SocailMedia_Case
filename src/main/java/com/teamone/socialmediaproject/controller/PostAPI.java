528f8b0d6295827bb6dce24b75c5c15c7e27a126
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-08-08 11:32:29 +0700

    showFrofileFriend

--- src/main/java/com/teamone/socialmediaproject/controller/PostAPI.java
+++ src/main/java/com/teamone/socialmediaproject/controller/PostAPI.java
@@ -59,7 +59,7 @@ public class PostAPI {
     public String upImg(@RequestParam MultipartFile file){
         String name = file.getOriginalFilename();
         try {
-            FileCopyUtils.copy(file.getBytes(),new File("C:\\Users\\ADMIN\\Desktop\\Case4\\FE_SocialMedia_Case\\assets\\images\\post\\imgpost\\" + name));
+            FileCopyUtils.copy(file.getBytes(),new File("D:\\CodeGym\\CaseModul4\\Social\\assets\\images\\post\\imgpost\\" + name));
         } catch (IOException e) {
             e.printStackTrace();
         }
